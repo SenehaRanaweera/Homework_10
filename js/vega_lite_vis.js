829f1ceb60b7ef6f7c03ff924797af136dfcf8f5
SenehaRanaweera <166363740+SenehaRanaweera@users.noreply.github.com>
2024-10-11 23:41:23 +1100

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,18 @@
+var vg_1 = "js/payment_methods_by_customers.json";
+var vg_2 = "js/Payment_method_according_to_gender.json";
+var vg_3 = "js/Complaints by providers.json";
+
+//Embed the fouth visualization
+vegaEmbed("#pie_chart", vg_1).then(function(result) {
+   
+}).catch(console.error);
+
+//Embed the fifth visualization
+vegaEmbed("#simple_bar_chart", vg_2).then(function(result) {
+   
+}).catch(console.error);
+
+// Embed the second visualization
+vegaEmbed("#stacked_bar_chart", vg_3).then(function(result) {
+   
+}).catch(console.error);
