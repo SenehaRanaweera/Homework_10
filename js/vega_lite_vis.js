54aea72d889be3ca08240b50a86ddbb1409a7f91
SenehaRanaweera <166363740+SenehaRanaweera@users.noreply.github.com>
2024-10-12 11:33:34 +1100

    Add files via upload

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,6 +1,6 @@
 var vg_1 = "js/payment_methods_by_customers.json";
 var vg_2 = "js/Payment_method_according_to_gender.json";
-var vg_3 = "js/Complaints by providers.json";
+var vg_3 = "js/Complaints_by_providers.json";
 
 //Embed the fouth visualization
 vegaEmbed("#pie_chart", vg_1).then(function(result) {
